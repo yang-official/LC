062dea1396f6edf28649b66df0a9978ecf9d2267
Your Name <yang.official@gmail.com>
2020-02-19 14:16:16 -0800

    sqlzoo start

--- SQL/N+1_Course/sqlzoo.sql
+++ SQL/N+1_Course/sqlzoo.sql
@@ -68,3 +68,14 @@ SELECT * FROM nobel WHERE winner = 'Eugene O\'neill'; -- escaping single quotes
 SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner; -- ORDER BY
 SELECT winner, subject FROM nobel WHERE yr = 1984 ORDER BY subject IN ('Physics','Chemistry'), subject, winner; -- numerical value of expressions
 -- Subqueries, SELECT within SELECT
+SELECT name FROM world WHERE population > (SELECT population FROM world WHERE name='Russia');
+SELECT name FROM world WHERE continent = 'Europe' AND gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom');
+SELECT name, continent FROM world WHERE continent IN (SELECT continent FROM world WHERE name in ('Argentina', 'Australia')) ORDER BY name;
+SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland');
+SELECT name, CONCAT(ROUND(population / (SELECT population FROM world WHERE name = 'Germany') * 100),'%') FROM world WHERE continent = 'Europe';
+-- using ALL
+SELECT name FROM world WHERE gdp > ALL (SELECT gdp FROM world WHERE continent = 'Europe' AND gdp IS NOT NULL);
+SELECT continent, name, area FROM world x WHERE area >= ALL (SELECT area FROM world y WHERE y.continent=x.continent AND area>0);
+SELECT continent, name FROM world x WHERE name <= ALL (SELECT name FROM world y WHERE x.continent = y.continent);
+SELECT name,continent,population FROM world x WHERE 25000000 >= ALL (SELECT population FROM world y WHERE x.continent=y.continent AND y.population>0);
+SELECT name, continent FROM world x WHERE population > ALL (SELECT population*3 FROM world y WHERE y.continent = x.continent AND y.name != x.name);
