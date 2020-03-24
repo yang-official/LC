9d72b072085df9307b2992aff135131dd2edc196
Your Name <yang.official@gmail.com>
2020-03-23 20:28:17 -0700

    practice

--- SQL/N+1_Course/sqlzoo.sql
+++ SQL/N+1_Course/sqlzoo.sql
@@ -122,7 +122,7 @@ SELECT matchid, mdate, COUNT(*) FROM game JOIN goal ON matchid = id WHERE teamid
 SELECT mdate, team1, SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1,
        team2, SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2
        FROM game LEFT JOIN goal ON (id = matchid) GROUP BY mdate,team1,team2 ORDER BY mdate, matchid, team1, team2;
-
+--
 
 -- Window functions
 SELECT party, votes,
