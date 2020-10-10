d9dac08f4551aa80435b6b41f98f9bdc2f36730b
Your Name <yang.official@gmail.com>
2020-10-10 14:46:05 -0700

    sqlzoo covid

--- SQL/N+1_Course/sqlzoo.sql
+++ SQL/N+1_Course/sqlzoo.sql
@@ -171,7 +171,6 @@ JOIN stops stopc ON c.stop=stopc.id
 JOIN stops stopd ON d.stop=stopd.id
 WHERE stopa.name='Craiglockhart' AND stopd.name='Lochend';
 
-
 -- Window functions
 SELECT lastName, party, votes
   FROM ge
@@ -218,3 +217,52 @@ SELECT party,COUNT(1)
    ) AS ed
 WHERE posn = 1
 GROUP BY party;
+
+--Window LAG
+SELECT name, DAY(whn), confirmed, deaths, recovered FROM covid
+WHERE name = 'Spain' AND MONTH(whn) = 3 ORDER BY whn;
+
+SELECT name, DAY(whn), confirmed, LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) AS dbf
+FROM covid WHERE name = 'Italy' AND MONTH(whn) = 3 ORDER BY whn;
+
+SELECT name, DAY(whn), confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) AS new
+FROM covid WHERE name = 'Italy' AND MONTH(whn) = 3 ORDER BY whn;
+
+SELECT name, DATE_FORMAT(whn,'%Y-%m-%d'), confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) AS 'new this week'
+FROM covid WHERE name = 'Italy' AND WEEKDAY(whn) = 0 ORDER BY whn;
+
+SELECT tw.name, DATE_FORMAT(tw.whn,'%Y-%m-%d'), tw.confirmed - lw.confirmed
+FROM covid tw LEFT JOIN covid lw ON
+  DATE_ADD(lw.whn, INTERVAL 1 WEEK) = tw.whn
+  AND tw.name=lw.name
+WHERE tw.name = 'Italy' AND WEEKDAY(tw.whn) = 0
+ORDER BY tw.whn;
+
+SELECT
+   name,
+   confirmed,
+   RANK() OVER (ORDER BY confirmed DESC) rc,
+   deaths,
+   RANK() OVER (ORDER BY deaths DESC) rc
+  FROM covid
+WHERE whn = '2020-04-20'
+ORDER BY confirmed DESC;
+
+SELECT
+   world.name,
+   ROUND(100000*confirmed/population,0),
+   RANK() OVER (ORDER BY confirmed/population) AS rank
+  FROM covid JOIN world ON covid.name=world.name
+WHERE whn = '2020-04-20' AND population > 10000000
+ORDER BY population DESC;
+
+SELECT name,DATE_FORMAT(whn,'%Y-%m-%d'), newCases AS peakNewCases
+FROM (
+  SELECT name,whn,newCases, RANK() OVER (PARTITION BY name ORDER BY newCases DESC) rnc
+  FROM (
+    SELECT name, whn,
+      confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) as newCases
+    FROM covid
+  ) AS x
+) AS y
+WHERE rnc = 1 AND newCases>1000 ORDER BY whn
